be485610022b1d1c3536231f6bbcc5c3427a6aa5
franciscovillaescusa <villaescusa.francisco@gmail.com>
2020-12-21 11:32:29 -0800

    update

--- Codes/pdf_nu_HR.py
+++ Codes/pdf_nu_HR.py
@@ -14,7 +14,7 @@ ptypes = [2]
 MAS    = 'CIC'
 do_RSD = False
 axis   = 0
-grid   = 1024
+grid   = 1500
 
 # smoothing parameters
 BoxSize = 1000.0 #Mpc/h
@@ -29,27 +29,27 @@ bins = 200
 z = {10:0, 9:0.5, 8:1, 7:2, 6:3, 5:4, 4:5, 3:6, 2:7, 1:8, 0:9}
 
 # do a loop over the different grid sizes
-for R in [5.0, 7.5, 10.0]:
+for R in [2.0, 3.0, 4.0, 5.0, 7.5, 10.0]:
 
     # compute FFT of the filter
     W_k = SL.FT_filter(BoxSize, R, grid, Filter, threads)
 
     # do a loop over the different neutrino masses
-    for cosmo,prefix in zip(['0_HR'], ['0.1eV_HR']):
+    for cosmo in ['0.1eV', '0.2eV', '0.4eV']:
 
         # do a loop over the different redshifts
         for snapnum in [10,9,8,7,6,5,4,3,2,1,0]:
 
             # get name of output file
-            fout = '../Results/Results_Gaussian_%s_%d_z=%s.hdf5'%(prefix,R,z[snapnum])
-            #if os.path.exists(fout):  continue
+            fout = '../Results/Results_Gaussian_%s_HR_%s_z=%s.hdf5'%(cosmo,R,z[snapnum])
+            if os.path.exists(fout):  continue
 
             # define the arrays containing the variance and the pdf of the fields
             var_tot = np.zeros(realizations,        dtype=np.float64)
             pdf_tot = np.zeros((realizations,bins), dtype=np.float64)
 
             # compute delta_max from snapshot
-            snapshot = '%s/%s/snapdir_%03d/snap_%03d'%(root,cosmo,snapnum,snapnum)
+            snapshot = '%s/%s/0_HR/snapdir_%03d/snap_%03d'%(root,cosmo,snapnum,snapnum)
             delta = MASL.density_field_gadget(snapshot, ptypes, grid, MAS, do_RSD, axis)
             delta = delta/np.mean(delta, dtype=np.float64)
             delta_smoothed = SL.field_smoothing(delta, W_k, threads) #smooth the field
